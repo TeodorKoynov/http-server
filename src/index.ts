d9013a0faa96fb911a31216a4159122fad004ae9
Teodor Koynov <teodor.koynov2005@gmail.com>
2026-01-06 16:28:19 +0200

    api route

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { handlerReadiness } from "./api/rediness.js";
 import { middlewareLogResponse, middlewareMetricsInc } from "./api/middleware.js";
 import { handlerMetrics } from "./api/metrics.js";
 import { handlerReset } from "./api/reset.js";
+import { handlerChirpsValidate } from "./api/chirps.js";
 
 const app = express();
 const PORT = 8080;
@@ -12,6 +13,8 @@ app.use(middlewareLogResponse)
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
 app.get("/api/healthz", handlerReadiness)
+app.post("/api/validate_chirp", handlerChirpsValidate)
+
 app.get("/admin/metrics", handlerMetrics)
 app.post("/admin/reset", handlerReset)
 
